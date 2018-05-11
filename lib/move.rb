e92d77c1817b21d72a4dfa3b3715fbc893174352
jaredianmills <jaredianmills@gmail.com>
2018-05-11 17:46:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +7,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
